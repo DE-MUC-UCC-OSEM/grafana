a6cd3fd9b746c1b511d916bd14db3b3ccede338c
Sven Beisiegel <162966719+svenbeisiegel@users.noreply.github.com>
2024-10-09 08:35:42 +0200

    Only modify a mounted grafana.template file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-if [ "$EXTERNAL_SERVER_ADDR" ]; then
-  sed -i -e "s/__EXTERNAL_SERVER_ADDR__/$EXTERNAL_SERVER_ADDR/" /etc/grafana/grafana.ini
-fi
-
-if [ "$INTERNAL_SERVER_ADDR" ]; then
-  sed -i -e "s/__INTERNAL_SERVER_ADDR__/$INTERNAL_SERVER_ADDR/" /etc/grafana/grafana.ini
+if [ -f /etc/grafana/grafana.template ]; then
+  cp /etc/grafana/grafana.template /etc/grafana/grafana.ini
+  if [ "$EXTERNAL_SERVER_ADDR" ]; then
+    sed -i -e "s/__EXTERNAL_SERVER_ADDR__/$EXTERNAL_SERVER_ADDR/" /etc/grafana/grafana.ini
+  fi
+  if [ "$INTERNAL_SERVER_ADDR" ]; then
+    sed -i -e "s/__INTERNAL_SERVER_ADDR__/$INTERNAL_SERVER_ADDR/" /etc/grafana/grafana.ini
+  fi
 fi
 
 exec "$@"
